19a3eb1345028b106cae34e9b81c38b20576f241
isong8 <isong8@my.centennialcollege.ca>
2022-03-24 13:23:31 -0400

    DeleteStudent first commit

--- graphql-react-client/src/components/DeleteStudent.js
+++ graphql-react-client/src/components/DeleteStudent.js
@@ -1,17 +1,73 @@
-
+import {gql, useQuery, useMutation } from "@apollo/client";
 import React, { Component }  from 'react';
+import './sample.css';
+import './addstyle.css'
 
-function DeleteStudent(props)
+const GET_STUDENTS = gql`
 {
+    students{
+        _id
+      email
+      firstName
+      lastName
+      program
+      
+    }
+}
+`;
 
+const DELETE_STUDENT = gql`
+mutation DeleteStudent(
+    $_id:String!){
+      deleteStudent(id:$_id){
+        _id
+      }
+    }  
+`;
 
-    return (
-        <div>
-            <h2> Delete Operation</h2>
-            <p>Complete the React code.</p>
+const DeleteStudent = ({ id }) => {
+    const { data } =  useQuery(GET_STUDENTS, { variables: { id }});
+    const [deleteStudent] = useMutation(DELETE_STUDENT);
+  
+    const handleOnClick = ()=> {
+        deleteStudent({ variables: {id }});
+    }
+
+    if (data && data.student) {
+        const { student: {id, firstName, lastName, program, email }} = data;
+
+        if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return<div>
+            
+            <table >
+                <tr>
+                        <th>_id</th>
+                        <th>email</th>
+                        <th>firstName</th>
+                        <th>lastName</th>
+                        <th>program</th>
+
+                </tr>
+                        <tr>
+                            <td>{_id}</td>
+                            <td>{email}</td>
+                            <td>{firstName}</td>
+                            <td>{lastName}</td>
+                            <td>{program}</td>
+
+                        </tr>
+            </table>
+            
+            <div class="center">
+            <button onClick={handleOnClick}>Remove</button>
+            </div>
+            
         </div>
-    );
-
+    }
+    return null;
 }
 
-export default DeleteStudent;
+
+export default DeleteStudent
